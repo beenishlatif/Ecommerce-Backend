d319fcbc5f00fd95458c7aed9a8b6a86d112c2f2
Beenish Latif <beenishlatif1026@gmail.com>
2026-08-30 15:19:54 +0500

    add

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { upload } from '../middleware/uploadMiddleware.js';
-import { uploadSingleImage, uploadMultipleImages } from '../controllers/uploadController.js';
+import {
+  uploadSingleImage,
+  uploadMultipleImages,
+  listGalleryImages,
+} from '../controllers/uploadController.js';
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+router.get('/gallery', protect, adminOnly, listGalleryImages);
 router.post('/', protect, adminOnly, upload.single('image'), uploadSingleImage);
 router.post('/multiple', protect, adminOnly, upload.array('images', 6), uploadMultipleImages);
 
